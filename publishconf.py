255aacc524544b1b650cc1be85c591cf553c5d81
otso-andersen <otso-andersen@gmail.com>
2018-10-24 09:12:57 +0200

    publish url

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = ''
+SITEURL = 'https://otso-andersen.github.io/website-template-pelican'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
